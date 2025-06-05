b3f14bb09fb321c6c50bf509f3a74239e783ccd6
samery46 <syamme@ymail.com>
2025-06-05 16:18:32 +0700

    Update akses list Resources 2

--- app/Filament/Resources/ApprovalSettingResource/Pages/ListApprovalSettings.php
+++ app/Filament/Resources/ApprovalSettingResource/Pages/ListApprovalSettings.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\ApprovalSettingResource\Pages;
 
 use App\Filament\Resources\ApprovalSettingResource;
+use App\Models\ApprovalSetting;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListApprovalSettings extends ListRecords
 {
@@ -15,5 +17,22 @@ class ListApprovalSettings extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTableQuery(): Builder
+    {
+
+    // $query = Network::query()->where('is_aktif', true); // Menambahkan filter agar hanya data aktif
+    $query = ApprovalSetting::query();
+    // Menerapkan filter berdasarkan akses plant_id pengguna
+    if (auth()->check() && auth()->user()->id === 1) {
+        // Jika user adalah admin, tidak ada filter tambahan
+    } else {
+        // Jika bukan admin, hanya ambil plant yang dimiliki oleh user
+        $userPlantIds = auth()->user()->plants->pluck('id')->toArray();
+        $query->whereIn('plant_id', $userPlantIds);
+    }
+
+    return $query;
     }
 }
--- app/Filament/Resources/CostCenterResource/Pages/ListCostCenters.php
+++ app/Filament/Resources/CostCenterResource/Pages/ListCostCenters.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\CostCenterResource\Pages;
 
 use App\Filament\Resources\CostCenterResource;
+use App\Models\CostCenter;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCostCenters extends ListRecords
 {
@@ -16,4 +18,22 @@ class ListCostCenters extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+
+public function getTableQuery(): Builder
+    {
+
+    // $query = Network::query()->where('is_aktif', true); // Menambahkan filter agar hanya data aktif
+    $query = CostCenter::query();
+    // Menerapkan filter berdasarkan akses plant_id pengguna
+    if (auth()->check() && auth()->user()->id === 1) {
+        // Jika user adalah admin, tidak ada filter tambahan
+    } else {
+        // Jika bukan admin, hanya ambil plant yang dimiliki oleh user
+        $userPlantIds = auth()->user()->plants->pluck('id')->toArray();
+        $query->whereIn('plant_id', $userPlantIds);
+    }
+
+    return $query;
+    }
+
 }
